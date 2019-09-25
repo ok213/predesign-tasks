f537ab384d7aa6c1131c2ce637142ebbe6128e4c
ok213 <213331@gmail.com>
2019-09-25 13:55:25 +0300

    application architecture changed

---
+++ task01/task01.sql
@@ -0,0 +1,18 @@
+ use task01;
+-- CREATE TABLE IF NOT EXISTS users (id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT, login VARCHAR(255), password VARCHAR(255), name VARCHAR(255));
+-- SHOW TABLES;
+-- INSERT INTO users(login, password, name) VALUES('aaa', 'aaa', 'aaa');
+-- INSERT INTO users(login, password, name) VALUES('bbb', 'bbb', 'bbb');
+-- INSERT INTO users(login, password, name) VALUES('ccc', 'ccc', 'ccc');
+
+-- DELETE FROM task01.users WHERE login='aaa' AND password='aaa' AND name='aaa';
+-- TRUNCATE TABLE task01.users;
+
+SELECT * FROM users;
+
+
+    
+    
+    
+    
+    
